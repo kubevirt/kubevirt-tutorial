b89275d75fd147279436548ab28cbd8d51553fff
karmab <karimboumedhel@gmail.com>
2018-08-02 12:52:46 +0200

    add our public keys to plan

--- administrator/kcli/requirements_kubernetes.sh
+++ administrator/kcli/requirements_kubernetes.sh
@@ -11,3 +11,4 @@ systemctl enable kubelet && systemctl start kubelet
 VERSION=v[[ kubernetes_version ]]
 IMAGES="k8s.gcr.io/kube-apiserver-amd64:$VERSION k8s.gcr.io/kube-scheduler-amd64:$VERSION k8s.gcr.io/kube-proxy-amd64:$VERSION k8s.gcr.io/kube-controller-manager-amd64:$VERSION k8s.gcr.io/etcd-amd64:3.1.12 k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3 k8s.gcr.io/k8s-dns-dnsmasq-nanny-amd64:1.14.8 k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.8 k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.8 k8s.gcr.io/pause-amd64:3.1 quay.io/coreos/flannel:v0.7.1-amd64"
 for image in $IMAGES; do docker pull $image ; done
+sed -i "/sleep 60/d" /root/requirements.sh
