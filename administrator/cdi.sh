3655fd6d2d26531ffe5d02ce52a46c459f9f4f53
karmab <karimboumedhel@gmail.com>
2018-10-23 20:46:40 +0200

    update to latest cdi

--- administrator/cdi.sh
+++ administrator/cdi.sh
@@ -1,6 +1,6 @@
-wget -P /root/ https://github.com/kubevirt/containerized-data-importer/releases/download/v1.2.0/cdi-controller.yaml
-wget -P /root https://github.com/kubevirt/containerized-data-importer/releases/download/v1.2.0/golden-pvc.yaml
-wget -P /root https://github.com/kubevirt/containerized-data-importer/releases/download/v1.2.0/endpoint-secret.yaml
+wget -P /root/ https://github.com/kubevirt/containerized-data-importer/releases/download/v1.3.0/cdi-controller.yaml
+wget -P /root https://github.com/kubevirt/containerized-data-importer/releases/download/v1.3.0/golden-pvc.yaml
+wget -P /root https://github.com/kubevirt/containerized-data-importer/releases/download/v1.3.0/endpoint-secret.yaml
 oc new-project golden
 sed -i "s/namespace:.*/namespace: golden/" cdi-controller.yaml
 oc adm policy add-scc-to-user privileged system:serviceaccount:golden:default
