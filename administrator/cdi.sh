3316afb14256a13aec18fb477e36c122b04cbb2a
karmab <karimboumedhel@gmail.com>
2018-11-08 13:54:58 +0100

    use latest stuff

--- administrator/cdi.sh
+++ administrator/cdi.sh
@@ -5,4 +5,4 @@ oc new-project golden
 sed -i "s/namespace:.*/namespace: golden/" cdi-controller.yaml
 oc adm policy add-scc-to-user privileged system:serviceaccount:golden:default
 oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:golden:cdi-apiserver
-oc create -f /root/cdi-controller-deployment.yaml
+oc create -f /root/cdi-controller.yaml
