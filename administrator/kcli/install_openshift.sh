555283cc3377fe4d72fd5ff9cb5efe7cfe72e4cc
karmab <karimboumedhel@gmail.com>
2019-01-09 23:50:17 +0100

    switch to openshift ansible

---
+++ administrator/kcli/install_openshift.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+yum -y install epel-release wget git bind-utils ansible.rpm
+git clone -b release-3.11 https://github.com/openshift/openshift-ansible /root/openshift-ansible
+mkdir /var/lib/etcd
+semanage fcontext -a -t container_file_t /var/lib/etcd
+restorecon -v /var/lib/etcd
+sed -i "s/#host_key_checking/host_key_checking = True/" /etc/ansible/ansible.cfg
+sed -i "s/#log_path/log_path/" /etc/ansible/ansible.cfg
+HOSTNAME=`hostname`.{{ domain }}
+sed -i "s/m01.default/$HOSTNAME/" /root/inventory
+sed -i "s/#openshift_master_default_subdomain=.*/openshift_master_default_subdomain=app.$HOSTNAME/" /root/inventory
+sed -i "s/#openshift_master_cluster_hostname=.*/openshift_master_cluster_hostname=$HOSTNAME/" /root/inventory
+sed -i "s/#openshift_master_cluster_public_hostname=.*/openshift_master_cluster_public_hostname=$HOSTNAME/" /root/inventory
+ansible-playbook -i /root/inventory /root/openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook -i /root/inventory /root/openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook -i /root/inventory /root/multus.yml
